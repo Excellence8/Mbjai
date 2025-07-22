1d6ab91d72083c3ae16a4015ab2b07d315ab78b8
Excellence8 <dgsjk3258@126.com>
2025-07-22 20:10:18 +0800

    fix: 确保CI文件末尾有换行符

---
+++ fix-yaml.ps1
@@ -0,0 +1,7 @@
+﻿# 修复项目中的YAML文件格式
+$yamls = Get-ChildItem .github/workflows/*.yml -Recurse
+foreach ($file in $yamls) {
+    $content = (Get-Content $file.FullName -Raw) -replace "(?m)^\s+$","`n"
+    [IO.File]::WriteAllText($file.FullName, $content.Trim() + "`n", [Text.Encoding]::UTF8)
+    Write-Host "已修复: $($file.FullName)"
+}
